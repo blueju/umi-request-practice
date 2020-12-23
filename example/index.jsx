68b2243b734128108a8472be4db24f8a3edd2ca4
blueju <>
2020-12-23 17:35:42 +0800

    update example

--- example/index.jsx
+++ example/index.jsx
@@ -1,4 +1,19 @@
 import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import request from 'umi-request-practice';
+
 export default () => {
-  return <h1 style={{ color: '#555' }}>Hello World!</h1>;
+  function handleSendRequest() {
+    request('').then(res => {
+      console.log(res);
+    });
+  }
+  return (
+    <>
+      <Button type="primary" onClick={handleSendRequest}>
+        发送请求
+      </Button>
+    </>
+  );
 };
